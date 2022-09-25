59a06b5db21aeb57e55e0b61b9e3c3398a36aac7
Niles O'Neil <nilesoneil@gmail.com>
2022-09-25 06:34:14 -0700

    added checkboxes to html

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,22 @@ Pizza.prototype.addToppings = function(toppings) {
   this.toppings += 1;
 }
 
+//UI Logic
+
+function handleForm(event) {
+  event.preventDefault();
+  const userSelections = document.querySelectorAll("input:checked");
+  const userSelectionsArray = Array.from(userSelections);
+
+  
+
+  userSelectionsArray.forEach(function(element) {
+    const paragraph = document.createElement("p");
+    paragraph.append(element.value);
+    document.body.append(paragraph);
+  });
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#transportation_survey").addEventListener("submit", handleForm);
+});
